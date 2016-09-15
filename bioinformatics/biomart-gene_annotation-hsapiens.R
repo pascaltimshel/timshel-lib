245dcc0ea60a318a45a6c02a5f8405a6d2b04887
Pascal Timshel OSX MPG1510 <pascal.timshel@gmail.com>
2016-09-15 12:08:05 +0200

    added biomart-gene_annotation-hsapiens.R and created a R markdown README file

--- bioinformatics/biomart-gene_annotation-hsapiens.R
+++ bioinformatics/biomart-gene_annotation-hsapiens.R
@@ -1,5 +1,5 @@
 ############### SYNOPSIS ###################
-# Mapping of gene lists: FROM ncbi entrez ids TO ensembl gene names
+# Get biomaRt annotation
 
 ### DESCRIPTION
 # ...
@@ -10,6 +10,8 @@
 ### REMARKS:
 # ....
 
+### REFERENCE:
+# Ensembl biomaRt guide: http://www.ensembl.org/info/data/biomart/biomart_r_package.html
 
 ############################################
 
@@ -22,24 +24,35 @@ library(dplyr)
 
 rm(list=ls())
 
-wd <- "XXXX"
+wd <- "<INSERT WORKING DIRECTORY HERE>"
 setwd(wd)
 
 
 # ======================================================================= #
 # ==============================  biomaRt  ============================== #
 # ======================================================================= #
+### Set version variable
+#ENSEMBL_VERSION = NULL # for newest version
+ENSEMBL_VERSION = 84 # for specific version
 
-### Inspect/explore the options for filters and attributes
-listMarts()
-ensembl <- useMart(biomart = "ensembl", dataset = "mmusculus_gene_ensembl") # IMPORTANT to save "mart"
+### List marts
+listMarts() #  list BioMart databases
+listEnsembl(verbose=T) # list BioMart databases hosted by Ensembl. 
+listEnsembl(version=ENSEMBL_VERSION)
+
+### List data sets
+listDatasets(mart=useEnsembl(biomart="ensembl", version=ENSEMBL_VERSION), verbose=T) # here we see the dataset "hsapiens_gene_ensembl" listed.
+
+### Connect to the BioMart database and dataset hosted by Ensembl
+ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", version=ENSEMBL_VERSION, verbose=T)
+
+
+### List options for filters and attributes
 attributePages(ensembl) 
   # ^ OBS: you can only qury a single "page" a at time using getBM(). 
   # I.e. you cannot get feature data and homolog data in a single getBM() call.
-tmp.attr <- listAttributes(ensembl) # ~1280 attributes
-tmp.attr
-tmp.filters <- listFilters(ensembl) # ~265 filers (e.g. entrezgene)
-tmp.filters
+ensembl.attr <- listAttributes(ensembl); ensembl.attr # ~1280 attributes
+ensembl.filters <- listFilters(ensembl); ensembl.filters # ~265 filers (e.g. entrezgene)
 filterType("entrezgene",ensembl)
 
 
@@ -48,9 +61,11 @@ filterType("entrezgene",ensembl)
 # ==============================   ALL GENES   ========================== #
 # ======================================================================= #
 
+# SEE "biomart-gene_annotation-mmusculus.R" for working with multiple pages
+
 ### Get all Ensembl Gene IDs
 df.BM.all.ids <- getBM(attributes = c("ensembl_gene_id"), mart=ensembl)
-nrow(df.BM.all.ids)# 48034
+nrow(df.BM.all.ids) # 66203 for ensembl_v84
 str(df.BM.all.ids)
 
 ### Features page
@@ -64,39 +79,23 @@ df.BM.all.feature <- getBM(attributes = c("ensembl_gene_id",
                                   "status",
                                   "description", 
                                   "name_1006"), mart=ensembl)
-  # ^^ name_1006: go name - takes a *loooong time* to download
+  # ^^ name_1006: GO name - takes a *loooong time* to download
 str(df.BM.all.feature)
-
-### Homolog page
-df.BM.all.homolog <- getBM(attributes = c("ensembl_gene_id", 
-                                  "hsapiens_homolog_ensembl_gene", 
-                                  "hsapiens_homolog_associated_gene_name",
-                                  "hsapiens_homolog_orthology_confidence"), mart=ensembl)
-str(df.BM.all.homolog)
-
+nrow(df.BM.all.feature) 
 
 ### Collapsing multi-entries - using dplyr summarise_each()
 df.BM.all.feature.collapsed <- df.BM.all.feature %>% group_by(ensembl_gene_id) %>% summarise_each(funs(paste0(unique(.), collapse=";"))) # collapse each "multi-row entry" for all columns in the data frame
-df.BM.all.homolog.collapsed <- df.BM.all.homolog %>% group_by(ensembl_gene_id) %>% summarise_each(funs(paste0(unique(.), collapse=";"))) # collapse each "multi-row entry" for all columns in the data frame
-### Collapsing multi-entries - did *NOT* work properly
-# df.x <- df.BM.all %>% group_by(ensembl_gene_id) %>% mutate(go=paste0(name_1006,collapse=";")) # not the thing I want. keeps the multi-row representation
+nrow(df.BM.all.feature.collapsed)
 
-### Checks
-any(duplicated(df.BM.all.feature.collapsed$ensembl_gene_id)) # --> False
-any(duplicated(df.BM.all.homolog.collapsed$ensembl_gene_id)) # --> False
-nrow(df.BM.all.feature.collapsed) # --> 22708
-nrow(df.BM.all.homolog.collapsed) # --> 48034
-
-
-###------------- Merge df.BM.all* ------------- ###
-### step1: we use the "df.BM.all.ids" to ensure that all ensembl_gene_id will be in the data frame
+### Merge df.BM.all ###
+### we use the "df.BM.all.ids" to ensure that all ensembl_gene_id will be in the data frame
 df.BM.all.merged.collapsed <- df.BM.all.ids %>% left_join(df.BM.all.feature.collapsed)
-### step2
-df.BM.all.merged.collapsed <- df.BM.all.merged.collapsed %>% left_join(df.BM.all.homolog.collapsed)
-###-------------------------------------------- ###
 
+### CHECKS
+stopifnot(nrow(df.BM.all.ids) == nrow(df.BM.all.merged.collapsed)) # *we want to make sure that we have all genes included*
 
 ### Write table
-file.ensmbl_annotation <- "biomaRt-annotation-mmusculus_gene_ensembl-v84.txt" 
-write.table(df.BM.all.merged.collapsed, file=file.ensmbl_annotation, col.names=T, row.names=F, quote=F, sep="\t")
-
+file.ensmbl_annotation <- sprintf("biomaRt-annotation-hsapiens_ensembl-v%s.txt.gz", ENSEMBL_VERSION)
+file.ensmbl_annotation.gz <- gzfile(file.ensmbl_annotation, 'w')
+write.table(df.BM.all.merged.collapsed, file=file.ensmbl_annotation.gz, col.names=T, row.names=F, quote=F, sep="\t")
+close(file.ensmbl_annotation.gz)
